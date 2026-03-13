a51b74991c6619c7c9ec8da5b1609c3362a5d84a
nalaejin-ux <nalaejin@gmail.com>
2026-03-14 08:59:39 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
+import React from 'react'
+
 export default function App() {
   return (
-    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', backgroundColor: '#ffffff', color: '#111111' }}>
       <h1>공부집사</h1>
-      <p>화면 테스트 성공</p>
+      <p>배포 화면 테스트 성공</p>
     </div>
   )
 }
